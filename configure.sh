67ddb5d6decd2113b1bc5bc54c7d046fadf58a43
Darktron <omyp12@gmail.com>
2025-03-06 02:51:00 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 # To change the cuda arch, edit Makefile.am and run ./build.sh
 
+#extracflags="-march=armv8-a+fp+simd+aes+sha2+crc+crypto -DARM"
+
 extracflags="-march=native"
 
 ./configure CXXFLAGS="$extracflags"
